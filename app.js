c9974505d6ccc6ca472a3d3f536d811266a04306
aron15951 <aronchoque72@gmail.com>
2024-06-24 12:21:18 -0500

    define la funcion para generar numeros aleatorios de nuestro juego

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-let titulo=document.querySelector('h1');
-titulo.innerHTML = 'Juego del numero secreto';
+let numeroSecreto = generarNumeroSecreto();
+console.log(numeroSecreto)
 
-let parrafo=document.querySelector('p');
-parrafo.innerHTML = 'Escoge un numero del 1 al 10'
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML=document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
 
 function intentoDeUsuario(){
     alert('Funciona');
+    return;
 }
+
+function generarNumeroSecreto(){
+    return Math.floor(Math.random()*10)+1;
+}
+
+
+asignarTextoElemento('h1', 'Juego del numero secreto');
+asignarTextoElemento('p', 'Escoge un numero del 1 al 10'); 
