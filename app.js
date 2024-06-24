08bffe7b6b552dead896a7ef48d5db616329c454
aron15951 <aronchoque72@gmail.com>
2024-06-24 10:46:49 -0500

    Define mi primera funcion en JS conectada con HTML

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo=document.querySelector('h1');
+titulo.innerHTML = 'Juego del numero secreto';
+
+let parrafo=document.querySelector('p');
+parrafo.innerHTML = 'Escoge un numero del 1 al 10'
+
+function intentoDeUsuario(){
+    alert('Funciona');
+}
