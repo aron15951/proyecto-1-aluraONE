b50b89eac21876f8cc00ed76b5d7b717bfda02c8
aron15951 <aronchoque72@gmail.com>
2024-06-26 11:11:59 -0500

    Implementa template string en el mensage deacertaste

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let numeroSecreto = generarNumeroSecreto();
-
+let intentos = 1;
 
 function asignarTextoElemento(elemento, texto){
     let elementoHTML=document.querySelector(elemento);
@@ -8,14 +8,17 @@ function asignarTextoElemento(elemento, texto){
 
 function verificarIntento(){
     let numeroUsuario = parseInt(document.getElementById('valorUsuario').value);
+
+    console.log(intentos);
     if (numeroUsuario === numeroSecreto) {
-        asignarTextoElemento('p', 'Acertaste el numero secreto');
+        asignarTextoElemento('p', `Acertaste el numero secreto en ${intentos} ${intentos === 1 ? 'intentos' : 'intentos'}`);
     } else {
         if (numeroUsuario > numeroSecreto) {
             asignarTextoElemento('p', 'El numero es menor');
         } else {
             asignarTextoElemento('p', 'El numero es mayor');
         }
+        intentos++; 
     }
     return;
 }
