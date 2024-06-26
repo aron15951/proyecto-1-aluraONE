74c29b5b43183376239d450a94a8a760558df627
aron15951 <aronchoque72@gmail.com>
2024-06-26 11:45:13 -0500

    Limpia el input mediante su id

--- app.js
+++ app.js
@@ -12,16 +12,24 @@ function verificarIntento(){
     console.log(intentos);
     if (numeroUsuario === numeroSecreto) {
         asignarTextoElemento('p', `Acertaste el numero secreto en ${intentos} ${intentos === 1 ? 'intentos' : 'intentos'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
+        //el usuario no ha acertado
         if (numeroUsuario > numeroSecreto) {
             asignarTextoElemento('p', 'El numero es menor');
         } else {
             asignarTextoElemento('p', 'El numero es mayor');
         }
         intentos++; 
+        limpiarInput();
     }
     return;
 }
+
+
+function limpiarInput(){
+    let input = document.querySelector('#valorUsuario').value = '';
+}   
 
 function generarNumeroSecreto(){
     return Math.floor(Math.random()*10)+1;
