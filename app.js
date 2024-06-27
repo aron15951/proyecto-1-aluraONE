46f3e017cc574b0a5a1c2f3a8b6aba7bcde16128
aron15951 <aronchoque72@gmail.com>
2024-06-27 15:34:02 -0500

    Recursividad

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+let listaNumerosSorteados = [];
 let numeroSecreto = generarNumeroSecreto();
 let intentos = 1;
+let numeroMaximo = 10;
+
 
 function asignarTextoElemento(elemento, texto){
     let elementoHTML=document.querySelector(elemento);
@@ -11,7 +14,7 @@ function verificarIntento(){
 
     console.log(intentos);
     if (numeroUsuario === numeroSecreto) {
-        asignarTextoElemento('p', `Acertaste el numero secreto en ${intentos} ${intentos === 1 ? 'intentos' : 'intentos'}`);
+        asignarTextoElemento('p', `Acertaste el numero secreto en ${intentos} ${intentos === 1 ? 'intento' : 'intentos'}`);
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         //el usuario no ha acertado
@@ -32,9 +35,37 @@ function limpiarInput(){
 }   
 
 function generarNumeroSecreto(){
-    return Math.floor(Math.random()*10)+1;
+    // si el número generado ya fue incluido en la lista
+    let numeroGenerado = Math.floor(Math.random() * numeroMaximo) + 1;
+
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    //si ya sorteamos todos los numeros
+    if (listaNumerosSorteados.length === numeroMaximo) {
+        asignarTextoElemento('p', 'Ya se sortearon todos los numeros posibles');
+    } else {
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+    
 }
 
 
-asignarTextoElemento('h1', 'Juego del numero secreto');
-asignarTextoElemento('p', 'Escoge un numero del 1 al 10'); 
+function mensajesIniciales(){
+    asignarTextoElemento('h1', 'Juego del numero secreto');
+    asignarTextoElemento('p', `Escoge un numero del 1 al ${numeroMaximo}`); 
+    numeroSecreto =  generarNumeroSecreto();
+    intentos = 1;
+}
+
+function reiniciarJuego(){
+    limpiarInput();
+    mensajesIniciales();
+    document.querySelector('#reiniciar').setAttribute('disabled', 'true');
+    
+}
+
